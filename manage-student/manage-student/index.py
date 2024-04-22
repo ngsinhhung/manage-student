ae0ce6dcb9e724807053d76730237fa1a8e48801
MinhTamNT <minhtam78945@gmail.com>
2024-04-22 12:55:18 +0700

    update

--- manage-student/manage-student/index.py
+++ manage-student/manage-student/index.py
@@ -2,12 +2,21 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def index():
     return render_template("login.html")
 
+@app.route("/inputScore")
+def input_score():
+    return render_template("inputScore.html")
+@app.route("/score_subject")
+def score_of_subject():
+    return render_template("inputScoreSubject.html")
+
+
+@app.route("/view_score")
+def view_score():
+    return render_template("viewScore.html")
 
 if __name__ == "__main__":
-    with app.app_context():
-        app.run(debug=True)
+    app.run(debug=True)
