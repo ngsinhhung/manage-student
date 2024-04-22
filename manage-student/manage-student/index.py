37c95faf3050f1cce1aeb1728b356b6ad3c8d524
Trần An Tiến <89237543+supertien12@users.noreply.github.com>
2024-04-22 16:15:50 +0700

    update layout

--- manage-student/manage-student/index.py
+++ manage-student/manage-student/index.py
@@ -3,9 +3,34 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+@app.route('/login')
+def login():
+    return render_template("login.html")
+
+
 @app.route('/')
 def index():
-    return render_template("login.html")
+    return render_template("index.html")
+
+
+@app.route('/create_class')
+def create_class():
+    return render_template("createclass.html")
+
+
+@app.route('/class_edit')
+def class_edit():
+    return render_template("list_class.html")
+
+
+@app.route('/register')
+def register():
+    return render_template("register.html")
+
+
+@app.route('/info/<class_id>')
+def info(class_id):
+    return render_template("class_info.html", class_id=class_id)
 
 
 if __name__ == "__main__":
