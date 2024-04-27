16caa1550dd4e23634c8d26e5175cfd0ac23e338
Trần An Tiến <89237543+supertien12@users.noreply.github.com>
2024-04-27 11:17:13 +0700

    update query student have no class

--- manage_student/manage_student/dao/auth.py
+++ manage_student/manage_student/dao/auth.py
@@ -1,5 +1,17 @@
-from manage_student.model import User
+import hashlib
+
+from manage_student.model import User, Profile
+
+
+def get_info_by_id(id):
+    return Profile.query.get(id)
 
 
 def load_user(user_id):
     return User.query.get(user_id)
+
+
+def auth_user(username, password):
+    password = str(hashlib.md5(password.strip().encode('utf-8')).hexdigest())
+    return User.query.filter(User.username.__eq__(username.strip()),
+                             User.password.__eq__(password)).first()
--- manage_student/manage_student/index.py
+++ manage_student/manage_student/index.py
@@ -1,30 +1,53 @@
-from flask import render_template, redirect, url_for
-from flask_login import current_user, login_required,logout_user
-from manage_student import app
+from flask import render_template, redirect, url_for,request
+from flask_login import current_user, login_required, logout_user, login_user
+from manage_student import app, login
+from manage_student.dao.auth import auth_user, get_info_by_id
+from manage_student.dao.student import create_student
 from manage_student.form import *
+from dao import auth
+from manage_student.model import UserRole
 
-app.config['SECRET_KEY'] = 'secretkey'
+
+@login.user_loader
+def user_load(user_id):
+    return auth.load_user(user_id)
+
+
+@app.route('/')
+def index():
+    if current_user.is_authenticated:
+        if current_user.user_role == UserRole.ADMIN:
+            return redirect("/admin")
+        return redirect(url_for('home'))
+    return redirect(url_for('login'))
+
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    mse = ""
     form = LoginForm()
-    if form.SubmitFieldLogin():
+    if request.method == "POST" and form.SubmitFieldLogin():
         username = form.username.data
-        password  = form.password.data
-        print(username)
-        print(password)
-    return render_template('login.html', form=form)
+        password = form.password.data
+        user = auth_user(username=username, password=password)
+        if user:
+            login_user(user)
+            return redirect(url_for("index"))
+        mse = "Tài khoản hoặc mật khẩu không đúng "
+    return render_template('login.html', form=form,mse=mse)
 
 
-@app.route("/logout/")
+@app.route("/log_out")
 def logout():
     logout_user()
     return redirect(url_for("login"))
 
 
-@app.route('/')
+@app.route('/home')
 @login_required
-def index():
-    return render_template("index.html")
+def home():
+    profile = get_info_by_id(current_user.id)
+    return render_template("index.html", profile=profile)
 
 
 @app.route('/teacher/assignment')
@@ -45,11 +68,14 @@ def class_edit():
     return render_template("list_class.html")
 
 
-@app.route('/student/create')
+@app.route('/student/register', methods=['GET', 'POST'])
 def register():
     form_student = AdmisionStudent()
-    if form_student.validate_on_submit():
-        pass
+
+    if request.method == "POST" and form_student.submit():
+        student = create_student(form_student)
+        if student:
+            return redirect(url_for("index"))
     return render_template("register_student.html",form_student=form_student)
 
 
@@ -76,4 +102,5 @@ def view_grade():
 
 if __name__ == "__main__":
     with app.app_context():
+        from manage_student import admin
         app.run(debug=True)
